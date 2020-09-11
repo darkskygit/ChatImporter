4984fe192c3dd7b551084b2927738d8650833a91
DarkSky <darksky2048@gmail.com>
2020-09-11 18:08:14 +0800

    parse wc contact

--- src/matcher/ios_wechat.rs
+++ src/matcher/ios_wechat.rs
@@ -1,15 +1,28 @@
 use super::*;
 use ibackuptool2::{Backup, BackupFile};
 use plist::Value;
+use rusqlite::{params, Connection, OpenFlags, Result as SqliteResult};
 use std::collections::HashMap;
-use std::io::Cursor;
+use std::io::{Cursor, Error, ErrorKind, Write};
+use std::sync::Arc;
+use tempfile::NamedTempFile;
 
-#[derive(Clone, Default, Debug)]
+#[derive(Clone)]
+struct Contact {
+    name: String,
+    remark: String,
+    head: String,
+    user_type: i32,
+}
+
+#[derive(Clone, Default)]
 struct UserDB {
-    contact: Option<BackupFile>,
-    message: Option<BackupFile>,
+    contact: Option<Arc<NamedTempFile>>,
+    message: Option<Arc<NamedTempFile>>,
     setting: Option<BackupFile>,
-    session: Option<BackupFile>,
+    session: Option<Arc<NamedTempFile>>,
+    account_files: HashMap<String, BackupFile>,
+    contacts: Vec<Contact>,
     account: String,
     wxid: String,
     name: String,
@@ -17,23 +30,49 @@ struct UserDB {
 }
 
 impl UserDB {
-    pub fn new(user_id: String, file: &BackupFile) -> Self {
-        let mut user_db = Self::default();
-        user_db.account = user_id;
-        user_db.match_path(file)
+    pub fn new(
+        backup: &Backup,
+        account: String,
+        file: &BackupFile,
+        account_files: Vec<BackupFile>,
+    ) -> Self {
+        let account_files = account_files
+            .iter()
+            .map(|file| (file.relative_filename.clone(), file.clone()))
+            .collect();
+        let user_db = Self {
+            account,
+            account_files,
+            ..Self::default()
+        };
+        user_db.match_path(backup, file)
     }
 
-    pub fn with(self, file: &BackupFile) -> Self {
-        self.match_path(file)
+    pub fn with(self, backup: &Backup, file: &BackupFile) -> Self {
+        self.match_path(backup, file)
     }
 
-    fn match_path(mut self, file: &BackupFile) -> Self {
-        match Path::new(&file.relative_filename).name_str() {
-            "WCDB_Contact.sqlite" => self.contact = Some(file.clone()),
-            "MM.sqlite" => self.message = Some(file.clone()),
-            "mmsetting.archive" => self.setting = Some(file.clone()),
-            "session.db" => self.session = Some(file.clone()),
-            _ => {}
+    fn match_path(mut self, backup: &Backup, file: &BackupFile) -> Self {
+        let filename = Path::new(&file.relative_filename).name_str().to_string();
+        if ["WCDB_Contact.sqlite", "MM.sqlite", "session.db"].contains(&filename.as_str()) {
+            if let Ok(tmpfile) = NamedTempFile::new().and_then(|mut tmpfile| {
+                let data = backup
+                    .read_file(file)
+                    .map_err(|e| Error::new(ErrorKind::Other, format!("{}", e)))?;
+                tmpfile.write_all(&data)?;
+                Ok(tmpfile)
+            }) {
+                match filename.as_str() {
+                    "WCDB_Contact.sqlite" => self.contact = Some(Arc::new(tmpfile)),
+                    "MM.sqlite" => self.message = Some(Arc::new(tmpfile)),
+                    "session.db" => self.session = Some(Arc::new(tmpfile)),
+                    _ => {}
+                }
+            } else {
+                warn!("Failed to extract file: {}", file.relative_filename);
+            }
+        } else if filename == "mmsetting.archive" {
+            self.setting = Some(file.clone());
         }
         self
     }
@@ -53,6 +92,12 @@ impl UserDB {
             );
         }
         ret
+    }
+
+    pub fn build(&mut self, backup: &Backup) -> Result<(), Box<dyn std::error::Error>> {
+        self.load_settings(backup)?;
+        self.load_contacts()?;
+        Ok(())
     }
 
     pub fn load_settings(&mut self, backup: &Backup) -> Result<(), Box<dyn std::error::Error>> {
@@ -93,6 +138,56 @@ impl UserDB {
             } else {
                 warn!("failed to load settings: {}", setting.relative_filename);
             }
+        }
+        Ok(())
+    }
+
+    fn get_conn(file: Option<Arc<NamedTempFile>>) -> SqliteResult<Option<Connection>> {
+        if let Some(file) = file {
+            Ok(Some(Connection::open_with_flags(
+                file.as_ref(),
+                OpenFlags::SQLITE_OPEN_READ_ONLY
+                    | OpenFlags::SQLITE_OPEN_URI
+                    | OpenFlags::SQLITE_OPEN_NO_MUTEX,
+            )?))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn load_contacts(&mut self) -> SqliteResult<()> {
+        if let Some(conn) = Self::get_conn(self.contact.clone())? {
+            self.contacts = conn
+                .prepare("SELECT userName, dbContactRemark, dbContactHeadImage, type FROM Friend")?
+                .query_map(params![], |row| {
+                    Ok(Contact {
+                        name: row.get(0)?,
+                        remark: {
+                            let remark: String = row.get(1)?;
+                            if remark.len() > 2
+                                && remark.chars().next() == Some('\n')
+                                && remark.chars().nth(1) >= Some('\0')
+                                && remark.chars().nth(1) <= Some('0')
+                            {
+                                let mut chars = vec![];
+                                for c in remark.chars().nth(2) {
+                                    if c < '\u{0012}' || c > '\u{0016}' {
+                                        chars.push(c);
+                                    } else {
+                                        break;
+                                    }
+                                }
+                                chars.iter().cloned().collect::<String>()
+                            } else {
+                                remark
+                            }
+                        },
+                        head: row.get(2)?,
+                        user_type: row.get(3)?,
+                    })
+                })?
+                .filter_map(|r| r.ok())
+                .collect();
         }
         Ok(())
     }
@@ -138,9 +233,20 @@ impl iOSWeChatExtractor {
                 {
                     if let Some(user) = user_map.remove(&user_id) {
                         let user: UserDB = user;
-                        user_map.insert(user_id, user.with(file));
+                        user_map.insert(user_id, user.with(&backup, file));
                     } else {
-                        user_map.insert(user_id.clone(), UserDB::new(user_id, file));
+                        user_map.insert(
+                            user_id.clone(),
+                            UserDB::new(
+                                &backup,
+                                user_id.clone(),
+                                file,
+                                backup.find_wildcard_paths(
+                                    DOMAIN,
+                                    &format!("Documents/{}/*", user_id),
+                                ),
+                            ),
+                        );
                     }
                 } else {
                     warn!("Unmatched path: {}", path.display());
@@ -154,9 +260,9 @@ impl iOSWeChatExtractor {
             .filter(|(_, user_db)| user_db.is_complete())
             .filter_map(|(user_id, user_db)| {
                 let mut user = user_db.clone();
-                user.load_settings(&backup)
+                user.build(&backup)
                     .map(|_| (user_id.clone(), user))
-                    .map_err(|e| warn!("failed to load settings: {}", e))
+                    .map_err(|e| warn!("failed to init user: {}", e))
                     .ok()
             })
             .collect()
